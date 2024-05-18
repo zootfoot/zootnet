17bcec7804b01995a94f86b0cbb14b4e3496d201
zootfoot <marsh@catboy.wtf>
2024-05-17 23:40:13 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: 'rgb(55, 153, 107)',
+          accent: 'rgb(62, 206, 141)',
+          white: 'rgb(237, 237, 237)',
+          gray: 'rgb(28, 28, 28)',
+          grayAccent: 'rgb(46, 46, 46)',
+          grayDark: 'rgb(18, 18, 18)'
+        },
+        fontFamily: {
+          poppins: ['Poppins', 'sans-serif'],
+          bungee: ['Bungee', 'cursive'],
+          russo: ['Russo One', 'sans-serif']
+        },
+        spacing: {
+          'pixel': '1px'
+        },
+      }
+    }
+  }
