71b58ce9fccd14dcc8bcd4e8cfa880bed3307cdd
zootfoot <github@zootfoot.com>
2024-07-31 02:49:17 -0400

    totally redid everything

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn src.app:app 0.0.0.0:$PORT
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__, static_folder='public', static_url_path='/public')
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+    SECRET_KEY=os.environ.get('SECRET_KEY'),
+    SESSION_TYPE='filesystem'
+)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
