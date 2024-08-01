d1e69e841ac97b03a9937b068f110dded86216ba
zootfoot <github@zootfoot.com>
2024-08-01 07:41:02 -0400

    now plots radar stations

--- public/js/wx.js
+++ public/js/wx.js
@@ -3,6 +3,47 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoiem9vdGZvb3QiLCJhIjoiY2x6YXcwdnN4MHNpZDJtcHk5d
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v11',
-    center: [-74.5, 40],
-    zoom: 9
+    center: [-98, 38],
+    projection: 'mercator',
+    zoom: 4
 });
+
+let selectedRadarBtn = null;
+
+async function fetchRadarStations() {
+    const response = await fetch('https://api.weather.gov/radar/stations');
+    const data = await response.json();
+    return data.features;
+}
+
+function addRadarSiteBtn(longitude, latitude, id, name) {
+    const radarSiteBtn = document.createElement('button');
+    radarSiteBtn.className = 'radar-site-button';
+    radarSiteBtn.innerHTML = id;
+
+    radarSiteBtn.onclick = () => {
+        if (selectedRadarBtn) {
+            selectedRadarBtn.classList.remove('radar-site-button-selected');
+        }
+        radarSiteBtn.classList.add('radar-site-button-selected');
+        selectedRadarBtn = radarSiteBtn;
+
+        console.log('Selected Radar Site:', name, id, [longitude, latitude]);
+    };
+
+    new mapboxgl.Marker(radarSiteBtn)
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+}
+
+async function initRadarSites() {
+    const features = await fetchRadarStations();
+    features.forEach(feature => {
+        const [longitude, latitude] = feature.geometry.coordinates;
+        const id = feature.properties.id;
+        const name = feature.properties.name;
+        addRadarSiteBtn(longitude, latitude, id, name);
+    });
+}
+
+initRadarSites();
